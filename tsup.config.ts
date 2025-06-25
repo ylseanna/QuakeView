3b1ce5f8e82b0a47c7eb00f9ff8d8d906d73a91a
ylseanna <ylseannadevries@gmail.com>
2025-06-25 16:18:59 +0000

    fix incomplete typescript compile before electron app is launched (disable cleaning)

--- tsup.config.ts
+++ tsup.config.ts
@@ -4,7 +4,7 @@ export default defineConfig({
   entry: ["./electron/main.ts", "./electron/preload.ts"],
   splitting: false,
   sourcemap: false,
-  clean: true,
+  clean: false,
   cjsInterop: true,
   skipNodeModulesBundle: true,
   treeshake: true,
