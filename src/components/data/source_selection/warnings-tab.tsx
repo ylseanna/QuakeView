3292778c8a2e023e0759b453add28ef328502e66
ylseanna <ylseannadevries@gmail.com>
2026-04-27 21:35:05 +0000

    remove dependencies

--- src/components/data/source_selection/warnings-tab.tsx
+++ src/components/data/source_selection/warnings-tab.tsx
@@ -1,38 +1,12 @@
 "use client";
 
-import { Edit, EditOff, Expand } from "@mui/icons-material";
-import {
-  Alert,
-  ClickAwayListener,
-  Collapse,
-  Divider,
-  IconButton,
-  Paper,
-  Stack,
-  styled,
-  TextField,
-  ToggleButton,
-  ToggleButtonGroup,
-  Tooltip,
-  tooltipClasses,
-  TooltipProps,
-  Box,
-  Typography,
-  useTheme,
-} from "@mui/material";
+import { Alert, Collapse, IconButton, Box, Typography } from "@mui/material";
 import AlertTitle from "@mui/material/AlertTitle";
 
-import { useFormatter, useTranslations } from "next-intl";
-
 import { useState } from "react";
-import { DataSource } from "../../custom/types";
-import { useProjectStore } from "@/providers/project-store-provider";
 import { useCatalogData } from "../use-data";
 import { ChevronDown } from "mdi-material-ui";
-
-interface DataTabProps {
-  dataSource: DataSource;
-}
+import { DataSource } from "@/components/custom/types";
 
 function WarningEntry({
   warningTitle,
@@ -44,7 +18,11 @@ function WarningEntry({
   const [open, setOpen] = useState(true);
 
   return (
-    <Alert key={warningTitle} severity="warning" sx={{ mb: 2, p: 2, py: 1, pb: 0}}>
+    <Alert
+      key={warningTitle}
+      severity="warning"
+      sx={{ mb: 2, p: 2, py: 1, pb: 0 }}
+    >
       <AlertTitle sx={{ mt: "-2px", width: "100%" }}>
         {warningTitle}
         <IconButton
@@ -58,46 +36,36 @@ function WarningEntry({
             transitionDuration: "150ms",
             transitionProperty: "transform",
           }}
-          onClick={()=>{setOpen(!open)}}
+          onClick={() => {
+            setOpen(!open);
+          }}
         >
           <ChevronDown />
         </IconButton>
       </AlertTitle>
       <Collapse in={open}>
-      <Box sx={{mb: 2.5}}>
-        {warnings.map((warningLine, index) => (
-          <Typography
-            variant="rawtext"
-            key={"warningKey" + index}
-            sx={{ fontSize: ".9rem" }}
-          >
-            {warningLine}
-            <br />
-          </Typography>
-        ))}</Box>
+        <Box sx={{ mb: 2.5 }}>
+          {warnings.map((warningLine, index) => (
+            <Typography
+              variant="rawtext"
+              key={"warningKey" + index}
+              sx={{ fontSize: ".9rem" }}
+            >
+              {warningLine}
+              <br />
+            </Typography>
+          ))}
+        </Box>
       </Collapse>
     </Alert>
   );
 }
 
-export default function DataSourceWarnings({ dataSource }: DataTabProps) {
-  const t = useTranslations();
-
-  const format = useFormatter();
-
-  const autosizeOptions = {
-    includeHeaders: true,
-    includeOutliers: true,
-    expand: true,
-  };
-
-  const { setName } = useProjectStore((state) => state.dataSourceActions);
-
-  const [amEditingName, setAmEditingName] = useState(false);
-  const [previewType, setPreviewType] = useState("parsed");
-
-  const theme = useTheme();
-
+export default function DataSourceWarnings({
+  dataSource,
+}: {
+  dataSource: DataSource;
+}) {
   const { data } = useCatalogData();
 
   if (data.byID[dataSource.internal_id]) {
